a8c6bd74fd448a0178126357a68a15d0d7726151
RjelAutentico <rodrigo.jara1901@alumnos.ubiobio.cl>
2022-07-30 17:50:44 -0400

    Las cajas de texto estan conectadas a la base de datos

--- save_task.php
+++ save_task.php
@@ -3,8 +3,12 @@
 include("db.php"); 
 
 if (isset($_POST['save_task'])){
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+    $rut_usuario = $_POST['rut_usuario'];
+    $cod_rol = $_POST['cod_rol'];
+    $nombre_per = $_POST['nombre_per'];
+    $apellido_per = $_POST['apellido_per'];
+    $telefono_per = $_POST['telefono_per'];
+    $num_domicilio = $_POST['num_domicilio'];
     
     
 }
