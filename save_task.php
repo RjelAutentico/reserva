cef0a5422d05655267fc87650db28ca4a62cd29b
RjelAutentico <rodrigo.jara1901@alumnos.ubiobio.cl>
2022-07-30 22:06:15 -0400

    Los datos introducidos terminan en phpMyAdmin

--- save_task.php
+++ save_task.php
@@ -9,8 +9,18 @@ if (isset($_POST['save_task'])){
     $apellido_per = $_POST['apellido_per'];
     $telefono_per = $_POST['telefono_per'];
     $num_domicilio = $_POST['num_domicilio'];
-    
-    
+
+    $query = "INSERT INTO usuario(rut_usuario, cod_rol, nombre_per, apellido_per, telefono_per, num_domicilio) 
+    VALUES ('$rut_usuario', '$cod_rol', '$nombre_per', '$apellido_per', '$telefono_per', '$num_domicilio')";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die("Query Failed");
+    }
+
+    header("Location: index.php");
+
+
 }
 
 ?>
