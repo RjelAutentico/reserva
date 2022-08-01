76b93f9daf8a609733f707073a3309d291e7cd47
RjelAutentico <rodrigo.jara1901@alumnos.ubiobio.cl>
2022-07-31 20:40:32 -0400

    Se agego una validacion de datos

--- save_task.php
+++ save_task.php
@@ -9,14 +9,16 @@ if (isset($_POST['save_task'])){
     $apellido_per = $_POST['apellido_per'];
     $telefono_per = $_POST['telefono_per'];
     $num_domicilio = $_POST['num_domicilio'];
+
     
 
 
     $query = "INSERT INTO usuario(rut_usuario, cod_rol, nombre_per, apellido_per, telefono_per, num_domicilio) 
     VALUES ('$rut_usuario', '$cod_rol', '$nombre_per', '$apellido_per', '$telefono_per', '$num_domicilio')";
-    $result = mysqli_query($conn, $query);
+    
+    /*$result = mysqli_query($conn, $query);*/
 
-    if (!$result) {
+    if (!$rut_usuario || !$nombre_per || !$apellido_per || !$telefono_per || !$num_domicilio) {
         die("Query Failed");
     }
 
