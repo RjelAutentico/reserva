18ee5461e3cba5d1767671bd8f9a6a74c9c0eb9f
RjelAutentico <105696082+RjelAutentico@users.noreply.github.com>
2022-08-04 22:26:05 -0400

    Se reparo un problema de conexión

--- save_task.php
+++ save_task.php
@@ -4,23 +4,21 @@ include("db.php");
 
 if (isset($_POST['save_task'])){
     $rut_usuario = $_POST['rut_usuario'];
-    $cod_rol = 3;
+    /*$cod_rol = 3;*/
     $nombre_per = $_POST['nombre_per'];
     $apellido_per = $_POST['apellido_per'];
     $telefono_per = $_POST['telefono_per'];
     $num_domicilio = $_POST['num_domicilio'];
 
-    
-
-
-    $query = "INSERT INTO usuario(rut_usuario, cod_rol, nombre_per, apellido_per, telefono_per, num_domicilio) 
-    VALUES ('$rut_usuario', '$cod_rol', '$nombre_per', '$apellido_per', '$telefono_per', '$num_domicilio')";
-    
-/*    $result = mysqli_query($conn, $query); */
-
     if (!$rut_usuario || !$nombre_per || !$apellido_per || !$telefono_per || !$num_domicilio) {
         die("No se llenaron todos los campos");
-
+    }
+    else{
+        $cod_rol = 3;
+        $query = "INSERT INTO usuario(rut_usuario, cod_rol, nombre_per, apellido_per, telefono_per, num_domicilio) 
+        VALUES ('$rut_usuario', '$cod_rol', '$nombre_per', '$apellido_per', '$telefono_per', '$num_domicilio')";
+    
+        $result = mysqli_query($conn, $query);
 
     }
 
@@ -33,5 +31,5 @@ if (isset($_POST['save_task'])){
 
 
 }
-
+/**//**/
 ?>
