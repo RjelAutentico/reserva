534837562d1368bb43f603a33235d55d2924651a
RjelAutentico <105696082+RjelAutentico@users.noreply.github.com>
2022-09-03 22:49:02 -0400

    Se pueden actualizar el nombre y rut

--- Formulario-2-main/edit.php
+++ Formulario-2-main/edit.php
@@ -7,9 +7,42 @@
 
         $result = mysqli_query($conn, $query);
         if(mysqli_num_rows($result) == 1){
-            echo 'puedes editar';
+            $row = mysqli_fetch_array($result);
+            $rut_usuario = $row['rut_usuario'];
+            $nombre_per = $row['nombre_per'];
+            $apellido_per = $row['apellido_per'];
+            $telefono_per = $row['telefono_per'];
+            $num_domicilio = $row['num_domicilio'];
+
         }
 
 
     }
 ?>
+
+
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" name="Rut Usuario" value="<?php echo $rut_usuario; ?>"
+                        class="form-control" placeholder="Actualizar Rut">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="Nombre" rows="2" class="form-control" placeholder="Actualizar Nombre"><?php echo $nombre_per; ?></textarea>
+                    </div>
+                    <button class="btn-success" name="update">
+                        Actualizar
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include("includes/footer.php") ?>
