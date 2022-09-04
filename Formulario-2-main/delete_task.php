8ac27ae634ee8edcb46efd2f1b1b1bff29a1022e
RjelAutentico <105696082+RjelAutentico@users.noreply.github.com>
2022-09-03 20:40:15 -0400

    Se empezó a agregar código para eliminar usuarios

--- Formulario-2-main/delete_task.php
+++ Formulario-2-main/delete_task.php
@@ -1,1 +1,10 @@
-<!---->
+<?php
+    include("conexion.php");
+
+    if(isset($_GET['rut_usuario'])) {
+        $id = $_GET['rut_usuario'];
+        $query = "DELETE FROM usuarios WHERE rut_usuario = $id";
+        $result = mysqli_query($conn, $query);
+
+    }
+?>
