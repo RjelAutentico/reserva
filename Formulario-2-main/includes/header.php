d9d1fdb8a04d2f3597ed77a72c619453b73e7c42
RjelAutentico <105696082+RjelAutentico@users.noreply.github.com>
2022-09-05 13:08:28 -0300

    Puede redirigir la pagina al inicio y a las reservas

--- Formulario-2-main/includes/header.php
+++ Formulario-2-main/includes/header.php
@@ -11,6 +11,7 @@
     
     <title>Usuarios</title>
 
+    
     <!-- BOostrap 4-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" 
     integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
@@ -36,6 +37,8 @@
 <nav class="navbar navbar-dark bg-dark">
     <div class="container">
         <h1>EdifRed</h1>
+        <a href="homepage.php" class="navbar-brand">P. INICIO</a>
+        <a href="homepagereservas.php" class="navbar-brand">RESERVAS</a>
         <a href="formulariousuario.php" class="navbar-brand">USUARIOS</a>
     </div>
 
