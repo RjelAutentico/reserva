8d8f2e93b86420f214129a7cbbeeaff943e3c6f9
RjelAutentico <105696082+RjelAutentico@users.noreply.github.com>
2022-09-05 23:53:50 -0300

    Ultimo Commit

--- Formulario-2-main/delete_task.php
+++ Formulario-2-main/delete_task.php
@@ -1,19 +1,6 @@
 <?php
     include("conexion.php");
 
-    // if(isset($_GET['rut_usuario'])) {
-    //     $id = $_GET['rut_usuario'];
-    //     $query = "DELETE FROM usuarios WHERE rut_usuario = '$id'";
-    //     $result = mysqli_query($conn, $query);
-    //     if (!$result)
-    //     {
-    //         die("Fallo en eliminar");
-    //     }
-
-    //     $_SESSION['message'] = 'Se removio con exito al usuario';
-    //     $_SESSION['message_type'] = 'danger';
-    //     header('Location: ../formulariousuario.php');
-    // }
 
     $id_rut=$_GET['id'];
 
--- Formulario-2-main/edit.php
+++ Formulario-2-main/edit.php
@@ -3,9 +3,9 @@
 
     if(isset($_GET['id'])) {
         $id = $_GET['id'];
-        $query = "SELECT * FROM usuarios WHERE rut_usuario = '$id'";
+        $consult = "SELECT * FROM usuarios WHERE rut_usuario = '$id'";
 
-        $result = mysqli_query($con, $query);
+        $result = mysqli_query($con, $consult);
         if(mysqli_num_rows($result) == 1){
             $row = mysqli_fetch_array($result);
             $rut_usuario = $row['rut_usuario'];
@@ -17,18 +17,21 @@
         }
 
 
+
+
     }
 
 
     if(isset($_POST['update'])) {
-        $rut_usuario = $_GET['rut_usuario'];
+        $rut_usuario = $_POST['rut_usuario'];
         $nombre_per = $_POST['nombre_per'];
         $apellido_per = $_POST['apellido_per'];
         $telefono_per = $_POST['telefono_per'];
         $num_domicilio = $_POST['num_domicilio'];
 
-        $query = "UPDATE usuarios set rut_usuario = '$rut_usuario', nombre_per = '$nombre_per', apellido_per = '$apellido_per', telefono_per = '$telefono_per',  num_domicilio = '$num_domicilio' WHERE rut_usuario = $rut_usuario";
-        mysqli_query($conn, $query);
+        $consult = "UPDATE usuarios set nombre_per = '$nombre_per', apellido_per = '$apellido_per', 
+        telefono_per = '$telefono_per',  num_domicilio = '$num_domicilio' WHERE rut_usuario = '$rut_usuario'";
+        $resultado = mysqli_query($con, $consult);
 
         $_SESSION['message'] = 'Se actualizo correctamente';
         $_SESSION['message_type'] = 'warning';
@@ -37,35 +40,38 @@
 ?>
 
 
+
+
+
 <?php include("includes/header.php") ?>
 
 <div class="container p-4">
     <div class="row">
         <div class="col-md-4 mx-auto">
             <div class="card card-body">
-                <form action="edit.php" method="POST" >
+                <form class="FORM" action="edit.php" method="POST" >
                     <div class="form-group">
-                        <input type="text" name="Rut Usuario" value="<?php echo $rut_usuario ?>"
+                        <input type="text" name="rut_usuario" disabled value="<?php echo $rut_usuario ?>"
                         class="form-control" placeholder="Actualizar Rut">
                     </div>
                    
                     <div class="form-group">
-                        <input name="Nombre" rows="2" class="form-control" placeholder="Actualizar Nombre" style="color: #000000"><?php echo $nombre_per ?></input>
+                        <input name="nombre_per" rows="2" class="form-control" placeholder="Actualizar Nombre" value="<?php echo $nombre_per ?>"></input>
                     </div>
                                        
                     <div class="form-group">
-                        <input name="Apellido" rows="2" class="form-control" placeholder="Actualizar Apellido"><?php echo $apellido_per ?></input>
+                        <input name="apellido_per" rows="2" class="form-control" placeholder="Actualizar Apellido" value="<?php echo $apellido_per ?>"></input>
                     </div>
                                        
                     <div class="form-group">
-                        <input name="Telefono" rows="2" class="form-control" placeholder="Actualizar Telefono"><?php echo $telefono_per ?></input>
+                        <input name="telefono_per" rows="2" class="form-control" placeholder="Actualizar Telefono" value="<?php echo $telefono_per ?>"></input>
                     </div>
                                        
                     <div class="form-group">
-                        <input name="Domicilio" rows="2" class="form-control" placeholder="Actualizar Domicilio"><?php echo $num_domicilio ?></input>
+                        <input name="num_domicilio" rows="2" class="form-control" placeholder="Actualizar Domicilio" value="<?php echo $num_domicilio ?>"></input>
                     </div>
                     
-                    <button class="btn btn-success" name="update">
+                    <button type="submit" class="btn btn-success" name="update">
                         Actualizar
                     </button>
                 </form>
